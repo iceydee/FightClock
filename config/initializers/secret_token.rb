80cc6ed893d277d877eacbb1439b63d63b765ba8
Mio Nilsson <mionilsson@gmail.com>
2010-11-02 19:05:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FightClock::Application.config.secret_token = '0e3f09dea9bc7f65a65c75d8a7f8fc41be22207205dc9e61e15d5a675393ef0d662179e77a03bde850c28f069a431c978d054fe9cef35da6284af7ea7ff621a9'
